86bd8d286013f9b340471d25b329a87fba1293a3
Taewan Kim <48190374+taewanek@users.noreply.github.com>
2020-08-11 11:12:14 +0900

    Add files via upload

--- markdowns/draw_heatmap.R
+++ markdowns/draw_heatmap.R
@@ -3,20 +3,24 @@
 # Import Part #
 
 ## Import the stored RData
-load(file = "Correlation_data.RData")
+load(file = "~/Box Sync/Correlation_data.RData")
 
 ## Import ggplot library to draw heatmap.
 library(ggplot2) # Visualization tool
 library(reshape2) # Matrix melting tool
 
 ## Store the matrices in a list and make a vector to label
-matrice <- list(cor_pearson_mat, cor_spearman_mat, faster_kendall_mat,
-                hoeff_dist, dist_cor_mat)
-label <- c("Pearson", "Spearman", "Kendall", "Hoeff", "Dist")
+matrices <- list(cor_pearson_mat, cor_spearman_mat, faster_kendall_mat,
+                cor_hoeffd_mat, cor_dist_mat, cor_MI_mat,
+                cor_MIC_mat, cor_XI_mat)
+abs_matrices <- list(cor_pearson_mat, cor_spearman_mat, faster_kendall_mat,
+                     cor_hoeffd_mat)
+label <- c("Pearson", "Spearman", "Kendall", "Hoeff",
+           "Dist", "MI", "MIC", "XI")
 
 # Drawing heatmap Part #
 
-for (i in 1:length(matrice)){
+for (i in 1:length(matrices)){
   cor_mat <- matrice[[i]]
   ## melt the matrix to draw a heatmap.
   melted_matrix <- melt(cor_mat, na.rm = T)
@@ -25,7 +29,10 @@ for (i in 1:length(matrice)){
   cor_heatmap <- ggplot(data = melted_matrix, 
                         aes(x = Var2, y = Var1, fill = value)) + 
     geom_tile() +
-    scale_fill_viridis_b(option = "D", direction = -1) +
+    scale_fill_viridis_b(breaks = round(as.numeric(
+      quantile(melted_matrix[,3],
+               probs = seq(0, 1,length.out = 6))), 1),
+      option = "D", direction = -1) +
     theme(axis.text.x = element_blank(),
           axis.text.y = element_blank(),
           axis.ticks.x = element_blank(),
@@ -33,7 +40,7 @@ for (i in 1:length(matrice)){
           axis.title.x = element_blank(),
           axis.title.y = element_blank(),
           panel.background = element_rect(fill = "transparent")) +
-    labs(title = paste0("Heatmap of ", label[i], " Correlation")) 
+    labs(title = paste0("Heatmap of ", label[i], " Correlation"))
   
   ## Store the heatmap as a file
   jpeg(filename = paste0("Heatmap_", label[i], "Corr.jpeg"),
@@ -45,8 +52,8 @@ for (i in 1:length(matrice)){
 
 # Drawing heatmaps of absolute values Part #
 
-for (i in 1:length(matrice)){
-  cor_mat <- abs(matrice[[i]])
+for (i in 1:length(abs_matrices)){
+  cor_mat <- abs(abs_matrices[[i]])
   ## melt the matrix to draw a heatmap.
   melted_matrix <- melt(cor_mat, na.rm = T)
   
@@ -54,7 +61,10 @@ for (i in 1:length(matrice)){
   cor_heatmap <- ggplot(data = melted_matrix, 
                         aes(x = Var2, y = Var1, fill = value)) + 
     geom_tile() +
-    scale_fill_viridis_b(option = "D", direction = -1) +
+    scale_fill_viridis_b(breaks = round(as.numeric(
+      quantile(melted_matrix[,3],
+               probs = seq(0, 1,length.out = 6))), 1),
+      option = "D", direction = -1) +
     theme(axis.text.x = element_blank(),
           axis.text.y = element_blank(),
           axis.ticks.x = element_blank(),
@@ -71,7 +81,3 @@ for (i in 1:length(matrice)){
   print(cor_heatmap)
   dev.off()
 }
-
-
-
-
